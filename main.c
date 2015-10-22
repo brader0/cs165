1e5e52cd9c72da98cfcb62f5da8a6e837822ecc3
Erin Marie Bradley <brader01@8282.local>
2015-10-22 14:29:45 -0500

    add main function

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
-Hi
+#include <stdio.h>
+#include "findup.h" 
+
+int main(int argc, char* argv[]) {
+
+  char c;
+
+  while ((c=getchar()) != EOF) {
+
+    if (findup(c) != 0)
+      c = c + 32;
+    }
+
+    printf("%c",c);
+  
+    
+}    
